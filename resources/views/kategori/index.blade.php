bc6f1c0a95cc03108287946d20f2dbc02479e7dc
chrisnoel485 <51897833+chrisnoel485@users.noreply.github.com>
2020-04-13 11:57:14 +0700

    add adminlte

--- resources/views/kategori/index.blade.php
+++ resources/views/kategori/index.blade.php
@@ -29,35 +29,6 @@
                         <div class="col-md-8">
                             <div class="card">
                                 <div class="card-header with-border">
-                                    <h3 class="card-title">Tambah</h3>
-                                </div>
-                                <div class="card-body">
-                                    <form role="form" action="{{ route('kategori.store') }}" method="POST">
-                                        @csrf
-                                        <div class="form-group">
-                                            <label for="name">Kategori</label>
-                                            <input type="text" 
-                                                name="name"
-                                                class="form-control {{ $errors->has('name') ? 'is-invalid':'' }}" id="name" required>
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="description">Deskripsi</label>
-                                            <textarea name="description" id="description" cols="5" rows="5" class="form-control {{ $errors->has('description') ? 'is-invalid':'' }}"></textarea>
-                                        </div>
-                                        <div class="card-footer">
-                                            <button class="btn btn-primary">Simpan</button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="col-md-8">
-                            <div class="card">
-                                <div class="card-header with-border">
                                     <h3 class="card-title">List Kategori</h3>
                                 </div>
                                 <div class="card-body">
