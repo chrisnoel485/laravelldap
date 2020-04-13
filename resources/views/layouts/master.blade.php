bf3811df0a04b9fcdb67b5083d2963c536e92709
chrisnoel485 <51897833+chrisnoel485@users.noreply.github.com>
2020-04-13 11:46:09 +0700

    add adminlte

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,17 +4,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-
     @yield('title')
-    
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="{{asset('lte/plugins/fontawesome-free/css/all.min.css')}}">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-
-    @yield('css')
 </head>
 <body class="hold-transition sidebar-mini">
 	<div class="wrapper">
