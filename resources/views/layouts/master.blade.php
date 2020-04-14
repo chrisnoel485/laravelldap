7e5db71d4a69fd03fd369a985b63ef6c607815ec
chrisnoel485 <51897833+chrisnoel485@users.noreply.github.com>
2020-04-14 10:21:14 +0700

    add adminlte

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -79,21 +79,6 @@
     <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
     <!-- AdminLTE App -->
     <script src="{{ asset('dist/js/adminlte.min.js')}}"></script>
-    <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('plugins/morris/morris.min.js') }}"></script>
-    <script src="{{ asset('plugins/sparkline/jquery.sparkline.min.js') }}"></script>
-    <script src="{{ asset('plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
-    <script src="{{ asset('plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
-    <script src="{{ asset('plugins/knob/jquery.knob.js') }}"></script>
-    <script src="{{ asset('plugins/daterangepicker/daterangepicker.js') }}"></script>
-    <script src="{{ asset('plugins/datepicker/bootstrap-datepicker.js') }}"></script>
-    <script src="{{ asset('plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') }}"></script>
-    <script src="{{ asset('plugins/slimScroll/jquery.slimscroll.min.js') }}"></script>
-    <script src="{{ asset('plugins/fastclick/fastclick.js') }}"></script>
-    <script src="{{ asset('dist/js/adminlte.js') }}"></script>
-    <script src="{{ asset('dist/js/pages/dashboard.js') }}"></script>
-    <script src="{{ asset('dist/js/demo.js') }}"></script>
 
     @yield('js')
 </body>
