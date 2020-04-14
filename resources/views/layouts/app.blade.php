d510616db7f64bd7aaeb8d5d381f42592b26e941
chrisnoel485 <51897833+chrisnoel485@users.noreply.github.com>
2020-04-14 15:24:33 +0700

    add Produk

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Management Asset') }}</title>
+    <title>Management Asset</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
