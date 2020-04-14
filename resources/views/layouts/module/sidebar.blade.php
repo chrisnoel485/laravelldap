2a3f45302078482ce2f94b737664c02afcf0bb4e
chrisnoel485 <51897833+chrisnoel485@users.noreply.github.com>
2020-04-14 22:17:15 +0700

    add role

--- resources/views/layouts/module/sidebar.blade.php
+++ resources/views/layouts/module/sidebar.blade.php
@@ -29,7 +29,7 @@
                         </p>
                     </a>
                 </li>
-                @if (auth()->user()->can('show products') || auth()->user()->can('delete products') || auth()->user()->can('create products'))
+                <!-- @if (auth()->user()->can('show products') || auth()->user()->can('delete products') || auth()->user()->can('create products'))-->
                 <li class="nav-item has-treeview">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fa fa-server"></i>
@@ -65,8 +65,8 @@
                         </li>
                     </ul>
                 </li>
-                @endif
-                @role('admin')
+                <!--@endif-->
+                <!--@role('admin')-->
                 <li class="nav-item has-treeview">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fa fa-users"></i>
@@ -90,7 +90,7 @@
                         </li>
                     </ul>
                 </li>
-                @endrole
+                <!--@endrole-->
                 <li class="nav-item has-treeview">
                     <a class="nav-link" href="{{ route('logout') }}"
                         onclick="event.preventDefault();
