31d7e1193c2009dc1c9fd33d8acc0a56c66a4f98
chrisnoel485 <51897833+chrisnoel485@users.noreply.github.com>
2020-04-14 21:30:19 +0700

    add role

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -26,55 +26,56 @@
         <section class="content">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-md-12">
-                        @card
-                            @slot('title')
-                            
-                            @endslot
-                            
-                            @if (session('error'))
-                                @alert(['type' => 'danger'])
-                                    {!! session('error') !!}
-                                @endalert
-                            @endif
-                            
-                            <form action="{{ route('users.store') }}" method="post">
-                                @csrf
-                                <div class="form-group">
-                                    <label for="">Nama</label>
-                                    <input type="text" name="name" class="form-control {{ $errors->has('name') ? 'is-invalid':'' }}" required>
-                                    <p class="text-danger">{{ $errors->first('name') }}</p>
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Add Users</h3>
+                            </div>
+                            <div class="card-body">
+                                @if (Session::has('message'))
+                                <div id="alert-msg" class="alert alert-success alert-dismissible">
+                                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                                        {{ Session::get('message') }}
                                 </div>
-                                <div class="form-group">
-                                    <label for="">Email</label>
-                                    <input type="email" name="email" class="form-control {{ $errors->has('email') ? 'is-invalid':'' }}" required>
-                                    <p class="text-danger">{{ $errors->first('email') }}</p>
+                                @endif
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <form action="{{ route('users.store') }}" method="POST">
+                                            @csrf
+                                            <div class="form-group">
+                                                <label for="">Nama</label>
+                                                <input type="text" name="name" class="form-control {{ $errors->has('name') ? 'is-invalid':'' }}" required>
+                                                <p class="text-danger">{{ $errors->first('name') }}</p>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="">Email</label>
+                                                <input type="email" name="email" class="form-control {{ $errors->has('email') ? 'is-invalid':'' }}" required>
+                                                <p class="text-danger">{{ $errors->first('email') }}</p>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="">Password</label>
+                                                <input type="password" name="password" class="form-control {{ $errors->has('password') ? 'is-invalid':'' }}" required>
+                                                <p class="text-danger">{{ $errors->first('password') }}</p>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="">Role</label>
+                                                <select name="role" class="form-control {{ $errors->has('role') ? 'is-invalid':'' }}" required>
+                                                    <option value="">Pilih</option>
+                                                    @foreach ($role as $row)
+                                                    <option value="{{ $row->name }}">{{ $row->name }}</option>
+                                                    @endforeach
+                                                </select>
+                                                <p class="text-danger">{{ $errors->first('role') }}</p>
+                                            </div>
+                                            <div class="card-footer">
+                                                <a href="{{ URL::to('users') }}" class="btn btn-outline-info">Kembali</a>
+                                                <input type="submit" value="Proses" class="btn btn-primary pull-right">
+                                            </div>
+                                        </form>
+                                    </div>
                                 </div>
-                                <div class="form-group">
-                                    <label for="">Password</label>
-                                    <input type="password" name="password" class="form-control {{ $errors->has('password') ? 'is-invalid':'' }}" required>
-                                    <p class="text-danger">{{ $errors->first('password') }}</p>
-                                </div>
-                                <div class="form-group">
-                                    <label for="">Role</label>
-                                    <select name="role" class="form-control {{ $errors->has('role') ? 'is-invalid':'' }}" required>
-                                        <option value="">Pilih</option>
-                                        @foreach ($role as $row)
-                                        <option value="{{ $row->name }}">{{ $row->name }}</option>
-                                        @endforeach
-                                    </select>
-                                    <p class="text-danger">{{ $errors->first('role') }}</p>
-                                </div>
-                                <div class="form-group">
-                                    <button class="btn btn-primary btn-sm">
-                                        <i class="fa fa-send"></i> Simpan
-                                    </button>
-                                </div>
-                            </form>
-                            @slot('footer')
-​
-                            @endslot
-                        @endcard
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -26,53 +26,54 @@
         <section class="content">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-md-12">
-                        @card
-                            @slot('title')
-                            
-                            @endslot
-                            
-                            @if (session('error'))
-                                @alert(['type' => 'danger'])
-                                    {!! session('error') !!}
-                                @endalert
-                            @endif
-                            
-                            <form action="{{ route('users.update', $user->id) }}" method="post">
-                                @csrf
-                                <input type="hidden" name="_method" value="PUT">
-                                <div class="form-group">
-                                    <label for="">Nama</label>
-                                    <input type="text" name="name" 
-                                        value="{{ $user->name }}"
-                                        class="form-control {{ $errors->has('name') ? 'is-invalid':'' }}" required>
-                                    <p class="text-danger">{{ $errors->first('name') }}</p>
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Edit Merek</h3>
+                            </div>
+                            <div class="card-body">
+                                @if (Session::has('message'))
+                                <div id="alert-msg" class="alert alert-success alert-dismissible">
+                                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                                        {{ Session::get('message') }}
                                 </div>
-                                <div class="form-group">
-                                    <label for="">Email</label>
-                                    <input type="email" name="email" 
-                                        value="{{ $user->email }}"
-                                        class="form-control {{ $errors->has('email') ? 'is-invalid':'' }}" 
-                                        required readonly>
-                                    <p class="text-danger">{{ $errors->first('email') }}</p>
+                                @endif
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <form role="form" action="{{ route('users.update', $user->id) }}" method="post">
+                                            @csrf
+                                            <input type="hidden" name="_method" value="PUT">
+                                            <div class="form-group">
+                                                <label for="">Nama</label>
+                                                <input type="text" name="name" 
+                                                    value="{{ $user->name }}"
+                                                    class="form-control {{ $errors->has('name') ? 'is-invalid':'' }}" required>
+                                                <p class="text-danger">{{ $errors->first('name') }}</p>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="">Email</label>
+                                                <input type="email" name="email" 
+                                                    value="{{ $user->email }}"
+                                                    class="form-control {{ $errors->has('email') ? 'is-invalid':'' }}" 
+                                                    required readonly>
+                                                <p class="text-danger">{{ $errors->first('email') }}</p>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="">Password</label>
+                                                <input type="password" name="password" 
+                                                    class="form-control {{ $errors->has('password') ? 'is-invalid':'' }}">
+                                                <p class="text-danger">{{ $errors->first('password') }}</p>
+                                                <p class="text-warning">Biarkan kosong, jika tidak ingin mengganti password</p>
+                                            </div>
+                                            <div class="card-footer">
+                                                <a href="{{ URL::to('merek') }}" class="btn btn-outline-info">Kembali</a>
+                                                <button class="btn btn-info">Update</button>
+                                            </div>
+                                        </form>
+                                    </div>
                                 </div>
-                                <div class="form-group">
-                                    <label for="">Password</label>
-                                    <input type="password" name="password" 
-                                        class="form-control {{ $errors->has('password') ? 'is-invalid':'' }}">
-                                    <p class="text-danger">{{ $errors->first('password') }}</p>
-                                    <p class="text-warning">Biarkan kosong, jika tidak ingin mengganti password</p>
-                                </div>
-                                <div class="form-group">
-                                    <button class="btn btn-primary btn-sm">
-                                        <i class="fa fa-send"></i> Update
-                                    </button>
-                                </div>
-                            </form>
-                            @slot('footer')
-​
-                            @endslot
-                        @endcard
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
