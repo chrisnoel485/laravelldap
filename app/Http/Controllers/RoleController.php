0c71e3ff7733c5283d07545857f4c21354302f04
chrisnoel485 <51897833+chrisnoel485@users.noreply.github.com>
2020-04-15 21:43:43 +0700

    add role

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -14,11 +14,11 @@ class RoleController extends Controller
         $role = Role::orderBy('created_at', 'DESC')->paginate(10);
         return view('role.index', compact('role'));
     }
-    
+
     public function create()
     {
-        $role = Role::orderBy('name', 'ASC')->get();
-        return view('users.create', compact('role'));
+        //
+        return view ('role/create');
     }
 
     public function store(Request $request)
@@ -29,35 +29,5 @@ class RoleController extends Controller
         
         $role = Role::firstOrCreate(['name' => $request->name]);
         return redirect()->back()->with(['success' => 'Role: <strong>' . $role->name . '</strong> Ditambahkan']);
-    }
-    
-    public function edit($id)
-    {
-        $user = User::findOrFail($id);
-        return view('users.edit', compact('user'));
-    }
-    
-    public function update(Request $request, $id)
-    {
-        $this->validate($request, [
-            'name' => 'required|string|max:100',
-            'email' => 'required|email|exists:users,email',
-            'password' => 'nullable|min:6',
-        ]);
-        
-        $user = User::findOrFail($id);
-        $password = !empty($request->password) ? bcrypt($request->password):$user->password;
-        $user->update([
-            'name' => $request->name,
-            'password' => $password
-        ]);
-        return redirect(route('users.index'))->with(['success' => 'User: <strong>' . $user->name . '</strong> Diperbaharui']);
-    }
-    
-    public function destroy($id)
-    {
-        $user = User::findOrFail($id);
-        $user->delete();
-        return redirect()->back()->with(['success' => 'User: <strong>' . $user->name . '</strong> Dihapus']);
-    }
+    }  
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use App\User;
 
 class UserController extends Controller
 {
