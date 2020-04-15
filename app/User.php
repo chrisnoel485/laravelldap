96fe95b97071195fc79557789eab491e12d2a389
chrisnoel485 <51897833+chrisnoel485@users.noreply.github.com>
2020-04-15 22:05:13 +0700

    add role

--- app/User.php
+++ app/User.php
@@ -5,13 +5,12 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-//use Spatie\Permission\Traits\HasRoles;
+use Spatie\Permission\Traits\HasRoles;
 
 
 class User extends Authenticatable
 {
-    use Notifiable;
-    //HasRoles;
+    use Notifiable,HasRoles;
 
     /**
      * The attributes that are mass assignable.
