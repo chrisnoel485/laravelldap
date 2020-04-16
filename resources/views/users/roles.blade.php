8bfc9f2ba068067044ae36b5975391fdc1bf1647
chrisnoel485 <51897833+chrisnoel485@users.noreply.github.com>
2020-04-16 11:50:36 +0700

    add role

--- resources/views/users/roles.blade.php
+++ resources/views/users/roles.blade.php
@@ -26,42 +26,47 @@
         <section class="content">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-md-6">
-                        <form action="{{ route('users.set_role', $user->id) }}" method="post">
-                            @csrf
-                            <input type="hidden" name="_method" value="PUT">
-                            <div class="table-responsive">
-                                <table class="table table-hover table-bordered">
-                                    <thead>
-                                        <tr>
-                                            <th>Nama</th>
-                                            <td>:</td>
-                                            <td>{{ $user->name }}</td>
-                                        </tr>
-                                        <tr>
-                                            <th>Email</th>
-                                            <td>:</td>
-                                            <td><a href="mailto:{{ $user->email }}">{{ $user->email }}</a></td>
-                                        </tr>
-                                        <tr>
-                                            <th>Role</th>
-                                            <td>:</td>
-                                            <td>
-                                                @foreach ($roles as $row)
-                                                <input type="radio" name="role" 
-                                                    {{ $user->hasRole($row) ? 'checked':'' }}
-                                                    value="{{ $row }}"> {{  $row }} <br>
-                                                @endforeach
-                                            </td>
-                                        </tr>
-                                    </thead>
-                                </table>
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Set Role</h3>
                             </div>
-                            <div class="card-footer">
-                                <a href="{{ URL::to('users') }}" class="btn btn-outline-info">Kembali</a>
-                                <input type="submit" value="Proses" class="btn btn-primary pull-right">
-                            </div>
-                        </form>
+                            <form action="{{ route('users.set_role', $user->id) }}" method="post">
+                                @csrf
+                                <input type="hidden" name="_method" value="PUT">
+                                <div class="table-responsive">
+                                    <table class="table table-hover table-bordered">
+                                        <thead>
+                                            <tr>
+                                                <th>Nama</th>
+                                                <td>:</td>
+                                                <td>{{ $user->name }}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Email</th>
+                                                <td>:</td>
+                                                <td><a href="mailto:{{ $user->email }}">{{ $user->email }}</a></td>
+                                            </tr>
+                                            <tr>
+                                                <th>Role</th>
+                                                <td>:</td>
+                                                <td>
+                                                    @foreach ($roles as $row)
+                                                    <input type="radio" name="role" 
+                                                        {{ $user->hasRole($row) ? 'checked':'' }}
+                                                        value="{{ $row }}"> {{  $row }} <br>
+                                                    @endforeach
+                                                </td>
+                                            </tr>
+                                        </thead>
+                                    </table>
+                                </div>
+                                <div class="card-footer">
+                                    <a href="{{ URL::to('users') }}" class="btn btn-outline-info">Kembali</a>
+                                    <input type="submit" value="Proses" class="btn btn-primary pull-right">
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
