11abc5f31237d0fd9d58cd9245629990d5a0ddad
chrisnoel485 <51897833+chrisnoel485@users.noreply.github.com>
2020-04-16 15:29:22 +0700

    add role

--- resources/views/layouts/module/sidebar.blade.php
+++ resources/views/layouts/module/sidebar.blade.php
@@ -3,7 +3,7 @@
     <a href="index3.html" class="brand-link">
         <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="POS" class="brand-image img-circle elevation-3"
             style="opacity: .8">
-        <span class="brand-text font-weight-light">POS</span>
+        <span class="brand-text font-weight-light">Management Asset</span>
     </a>
 ​
     <!-- Sidebar -->
