af19379494b4537d490ee91ae11501568518e9da
chrisnoel485 <51897833+chrisnoel485@users.noreply.github.com>
2020-04-30 16:21:46 +0700

    add role

--- routes/web.php
+++ routes/web.php
@@ -17,45 +17,10 @@ Route::get('/', function() {
 });
 Auth::routes();
 Route::group(['middleware' => 'auth'], function() {
-    
-    //Route yang berada dalam group ini hanya dapat diakses oleh user
-    //yang memiliki role admin
-  // Route::group(['middleware' => ['role:admin']], function () {
-        Route::resource('/role', 'RoleController')->except([
-            'show', 'edit', 'update'
-       ]);
-        
-        Route::resource('/users', 'UserController')->except([
-            'show'
-        ]);
-        Route::get('/users/roles/{id}', 'UserController@roles')->name('users.roles');
-        Route::put('/users/roles/{id}', 'UserController@setRole')->name('users.set_role');
-        Route::post('/users/permission', 'UserController@addPermission')->name('users.add_permission');
-        Route::get('/users/role-permission', 'UserController@rolePermission')->name('users.roles_permission');
-        Route::put('/users/permission/{role}', 'UserController@setRolePermission')->name('users.setRolePermission');
-    //});
-    
-    
-    //route yang berada dalam group ini, hanya bisa diakses oleh user
-    //yang memiliki permission yang telah disebutkan dibawah
-    //Route::group(['middleware' => ['permission:produk']], function() {
-        Route::resource('/kategori', 'KategoriController')->except([
-            'show'
-        ]);
-        Route::resource('/lokasi', 'LokasiController')->except([
-            'show'
-        ]);
-        Route::resource('/merek', 'MerekController')->except([
-            'show'
-        ]);
-        Route::resource('/produk', 'ProdukController');
-    //});
-    
-    //route group untuk kasir
-    Route::group(['middleware' => ['role:kasir']], function() {
-        
-    });
-    
-    //home kita taruh diluar group karena semua jenis user yg login bisa mengaksesnya
+    Route::resource('/kategori', 'KategoriController')->except([
+         'show'
+    ]);
+    Route::resource('/produk', 'ProdukController');
     Route::get('/home', 'HomeController@index')->name('home');
+    
 });
