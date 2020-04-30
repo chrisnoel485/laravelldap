1555b9007752340e420c333f9233f5244055e8fd
chrisnoel485 <51897833+chrisnoel485@users.noreply.github.com>
2020-04-30 23:13:56 +0700

    add role

--- resources/views/users/roles.blade.php
+++ resources/views/users/roles.blade.php
@@ -10,9 +10,6 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0 text-dark">Set Role</h1>
-                    </div>
-                    <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
                             <li class="breadcrumb-item"><a href="{{ route('users.index') }}">User</a></li>
